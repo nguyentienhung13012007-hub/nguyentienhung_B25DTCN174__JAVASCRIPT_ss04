64fa1f07156a54b0f1a89e70aab50bb969ab2a76
nguyentienhung13012007-hub <nguyentienhung13012007@gmail.com>
2026-02-02 07:38:38 +0700

    Add files via upload

---
+++ kt30p.js
@@ -0,0 +1,36 @@
+let n=-5;
+// 1.	Kiểm tra xem n là số chẵn hay số lẻ. In ra thông báo: "Số [n] là số chẵn" hoặc "Số [n] là số lẻ". 
+
+if (n%2===0) {
+    alert(`Số ${n} là số chẵn`);
+
+    
+}else{
+    alert(`Số ${n} là số lẻ`);
+    
+}
+//2.	Kiểm tra xem n là số dương, số âm hay bằng 0. 
+if(n>0){
+    console.log(`${n}  là số dương`);
+    
+}else if(n<0){
+    console.log(` ${n} là số âm`);
+    
+}else {
+    console.log(`${n} bằng 0`);
+    
+}
+    
+
+// 3.	Nếu n > 0, sử dụng vòng lặp để in ra các số từ 1 đến n. Nếu n <= 0, in ra thông báo: "Giá trị n không hợp lệ để tạo dãy số".
+
+if(n>0){
+    for(let i=1;i<=n;i++){
+        console.log("số từ 1 đến n: ",i);
+        
+
+    }
+}else{
+    console.log(`Giá trị ${n} không hợp lệ để tạo dãy số`);
+    
+}
---
+++ kt30p_2.js
@@ -0,0 +1,18 @@
+// Viết một chương trình duyệt các số tự nhiên từ 1 đến 50.
+let Fizz=0;
+for(let i=1;i<=50;i++){
+    if(i%3==0){
+        console.log(`${i} là  :Fizz`);
+        Fizz++;
+    }else if(i%5==0){
+        console.log(`${i} là  :Buzz`);
+        
+    }else if(i%3==0 && i%5==0){
+        console.log(`${i} là  :FizzBuzz`);
+        
+    }else{
+        console.log(i);
+        
+    }
+}
+console.log("Tổng các số Fizz: ",Fizz);
